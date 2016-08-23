ee9bf50919cf9ad2452d43b2b22578565c32ae82
Preovaleo <theo.bouge@gmail.com>
2016-08-23 14:19:08 +0200

    Del session manager

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var session = require('express-session')
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -23,14 +22,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-//session
-app.use(session({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: true }
-}))
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,16 @@ var express = require('express');
 var router = express.Router();
 var models = require('../models');
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
 
-    if (typeof req.session.userId === "undefined") {
-        res.redirect('/users/login');
-    } else {
-        models.User.findAll({
-            include: [models.Contact]
-        }).then(function (users) {
-            res.render('index', {
-                title: 'Sequelize: Express Example',
-                users: users
-            });
+    models.User.findAll({
+        include: [models.Contact]
+    }).then(function (users) {
+        res.render('index', {
+            title: 'Sequelize: Express Example',
+            users: users
         });
-    }
+    });
 });
 
 module.exports = router;
