1c2af21cc2792d1c91fc2cf885f262e325cad3ca
Preovaleo <theo.bouge@gmail.com>
2016-09-03 21:54:53 +0200

    set getAll route

--- routes/apis/contact/getAll.js
+++ routes/apis/contact/getAll.js
@@ -3,7 +3,11 @@ var models = require('../../../models');
 module.exports = function (req, res) {
 
 
-    models.Contact.findAll().then(function (contacts) {
+    models.Contact.findAll({
+        where:{
+            UserId: req.user.id
+        }
+    }).then(function (contacts) {
         res.json(contacts);
     });
 };
